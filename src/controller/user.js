43305419c6e3ab2f3bdf1e12beab6cb9d5bf3cae
ratishjain <ratishjain6@gmail.com>
2024-01-15 23:24:10 +0530

    signup setup

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,5 +1,22 @@
 const User = require("../models/user");
-const Otpverification = require("../models/otpverification");
+const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
+const Otpverification = require("../models/UserOTPVerification");
+const otpgen = require("../utils/otpgenerator");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+    user: process.env.USER_EMAIL,
+    pass: "hwsz cqfm hydh omzn",
+  },
+});
 
 const handleSignUp = async (req, res) => {
   const { email, password, name } = req.body;
@@ -13,11 +30,53 @@ const handleSignUp = async (req, res) => {
       .json({ error: "User already exists with this email" });
   } else {
     //signup process
+    let saltRounds = 10;
+    bcrypt
+      .genSalt(saltRounds)
+      .then((salt) => {
+        return bcrypt.hash(password, salt);
+      })
+      .then(async (hash) => {
+        const user = await User.create({
+          name: name,
+          email: email,
+          password: hash.toString(),
+        });
+
+        sendOTPVerificationEmail(user, res);
+      })
+      .catch((err) => console.error(err.message));
   }
 };
 const handleOTPVerifcation = async (req, res) => {};
 const handleOTPResend = async (req, res) => {};
 
+const sendOTPVerificationEmail = async ({ _id, email }, res) => {
+  const otp = otpgen();
+  try {
+    const mailOptions = {
+      from: '"Ratish Jain" <ratishjain6@gmail.com>',
+      to: email,
+      subject: "otp verification",
+      text: "OTP VERIFICATION",
+      html: `OTP: <b>${otp}</b>`,
+    };
+
+    let saltRounds = 10;
+    const hashOTP = await bcrypt.hash(otp, saltRounds);
+    await Otpverification.create({
+      userId: _id,
+      otp: hashOTP.toString(),
+      createdAt: Date.now(),
+      expiresAt: Date.now() + 60000,
+    });
+
+    await transporter.sendMail(mailOptions);
+    res.json({ status: "pending", message: "Otp is not verified!!" });
+  } catch (err) {
+    res.json({ status: "error", message: err.message });
+  }
+};
 module.exports = {
   handleSignUp,
   handleOTPVerifcation,
